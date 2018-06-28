b551eeb70edb0fbb26a010f9cb90858be3cf3012
PhillChris <chrph@utschools.ca>
2018-06-27 22:24:06 -0400

    added empty BusStation and SubwayStation classes, as well as Station interface, and created simple .gitignore

---
+++ phase1/src/BusStation.java
@@ -0,0 +1,13 @@
+public class BusStation implements Station{
+  private String name;
+  private float  initialFee= 2.0f;
+  private SubwayStation associatedSubway;
+
+  public BusStation(String name) {
+
+  }
+
+  public void charge() {
+
+  }
+}
---
+++ phase1/src/SubwayStation.java
@@ -0,0 +1,13 @@
+public class SubwayStation implements Station {
+  private String name;
+  private float perStationCharge = 0.5f;
+  private BusStation associatedBuses;
+
+  public SubwayStation(String name) {
+
+  }
+
+  public void charge() {
+
+  }
+}

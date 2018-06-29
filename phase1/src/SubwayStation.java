ae01e9a590bf20ae07a5078442c6c103070fd23c
PhillChris <chrph@utschools.ca>
2018-06-29 13:50:12 -0400

    fixed typo in docstring for SubwayStation

--- phase1/src/SubwayStation.java
+++ phase1/src/SubwayStation.java
@@ -2,8 +2,8 @@ public class SubwayStation extends Station {
   /**
    * Constructs a new instance of SubwayStation.
    *
-   * @param name the name of this BusStation
-   * @param route the name of the route to which this BusStation belongs
+   * @param name the name of this SubwayStation
+   * @param route the name of the route to which this SubwayStation belongs
    */
   public SubwayStation(String name, String route) {
     this.name = name;

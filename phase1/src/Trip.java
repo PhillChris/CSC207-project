dea83b366cd134f4ee30f3a2eebd3d52294968af
Fardin Syed <fardin.syed99@gmail.com>
2018-06-28 22:15:46 -0400

    Added attributes and blank methods to Trip

--- phase1/src/Trip.java
+++ phase1/src/Trip.java
@@ -1,2 +1,32 @@
+import java.util.Date;
+
+/** Represents an object of Trip */
 public class Trip {
+    Date timeStarted;
+    Date timeEnded;
+    Station startStation;
+    Station endStation;
+    float tripFee;
+
+
+    public Trip(Date startTime, Station startStation){
+
+    }
+
+    void endTrip(Station endStation){
+
+    }
+
+    void addFare(float value){
+
+    }
+
+    boolean isValidRoute (Station endStation) {
+        return true;
+    }
+
+    Station getStartStation(){
+        Station station = new BusStation("hello", "world");
+        return station;
+    }
 }

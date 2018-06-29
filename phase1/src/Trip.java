9b07c40ed172f10f66375cbbc2b0021d119bb96a
Fardin Syed <fardin.syed99@gmail.com>
2018-06-28 23:18:39 -0400

    Updates getStartStation in Trip

--- phase1/src/Trip.java
+++ phase1/src/Trip.java
@@ -2,61 +2,54 @@ import java.util.Date;
 
 /** Represents an object of Trip */
 public class Trip {
-    static final double MAXFEE = 6.0;
-    static final double MAXTRIPLENGTH = 2.0;
-    Date timeStarted;
-    Date timeEnded;
-    Station startStation;
-    Station endStation;
-    double tripFee;
+  static final double MAXFEE = 6.0;
+  static final double MAXTRIPLENGTH = 2.0;
+  Date timeStarted;
+  Date timeEnded;
+  Station startStation;
+  Station endStation;
+  double tripFee;
 
-    /**
-     * Construct a new instance of Trip
-     * @param startTime Time which the trip is started
-     * @param station Station which the trip is started
-     */
-    public Trip(Date startTime, Station station){
-        timeStarted = startTime;
-        startStation = station;
-        tripFee = station.getInitialFee();
+  /**
+   * Construct a new instance of Trip
+   *
+   * @param startTime Time which the trip is started
+   * @param station Station which the trip is started
+   */
+  public Trip(Date startTime, Station station) {
+    timeStarted = startTime;
+    startStation = station;
+    tripFee = station.getInitialFee();
+  }
+
+  void endTrip(Station endStation) {}
+
+  void addFee(double value) {}
+
+  double getFee() {
+    if (tripFee < MAXFEE) {
+      return tripFee;
+    } else {
+      return MAXFEE;
     }
+  }
 
-
-    void endTrip(Station endStation){
-
+  /**
+   * @param newStation new station
+   * @param time time for the start of the continuation
+   * @return true if continuous trip, false otherwise
+   */
+  boolean isContinuousTrip(Station newStation, Date time) {
+    if (TransitSystem.routes[startStation.route].contains(newStation)
+        && timeStarted.getDate() == time.getDate()
+        && (time.getHours() - timeStarted.getHours() < MAXTRIPLENGTH)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    void addFee(double value){
-
-    }
-
-    double getFee(){
-        if (tripFee < MAXFEE){
-            return tripFee;
-        }
-        else{
-            return MAXFEE;
-        }
-    }
-
-    /**
-     * @param newStation new station
-     * @param time time for the start of the continuation
-     * @return true if continuous trip, false otherwise
-     */
-    boolean isContinuousTrip (Station newStation, Date time) {
-        if (TransitSystem.routes[startStation.route].contains(newStation) &&
-                timeStarted.getDate() == time.getDate() &&
-                (time.getHours() - timeStarted.getHours() < MAXTRIPLENGTH)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    Station getStartStation(){
-        Station station = new BusStation("hello", "world");
-        return station;
-    }
+  Station getStartStation() {
+    return startStation;
+  }
 }

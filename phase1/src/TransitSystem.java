26918812149f99ea7b4d5a33c324db0ecc40687e
ren <rzhang0701@gmail.com>
2018-06-29 20:46:59 -0400

    Changed type declarations from ArrayList to List.

--- phase1/src/TransitSystem.java
+++ phase1/src/TransitSystem.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /** This wraps together all the contents of TransitSystem */
 public class TransitSystem {
-  private static HashMap<String, ArrayList<Station>> routes;
-  private static ArrayList<Trip> allTrip;
-  private static ArrayList<CardHolder> allUsers;
+  private static HashMap<String, List<Station>> routes;
+  private static List<Trip> allTrip;
+  private static List<CardHolder> allUsers;
   private static int month;
   private static int day;
 
@@ -13,7 +13,7 @@ public class TransitSystem {
     return 0;
   }
 
-  public static HashMap<String, ArrayList<Station>> getRoutes() {
+  public static HashMap<String, List<Station>> getRoutes() {
     return routes;
   }
 }

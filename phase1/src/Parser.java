5801499c134a50ab37e8830c772b8e52387e05d3
Fardin Syed <fardin.syed99@gmail.com>
2018-07-05 20:55:40 -0400

    Created Parser class with empty methods

---
+++ phase1/src/Parser.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Parser {
+
+    HashMap<String, Runnable> keyWords;
+
+    public static void Parse(){
+        
+    }
+
+
+    private static void tap(List<String> cardInfo){
+
+    }
+
+    private static void addUser(List<String> userInfo){
+
+    }
+
+    private static void addCard(List<String> cardInfo){
+
+    }
+
+    private static void removeCard(List<String> cardInfo){
+
+    }
+
+    private static void reportTheft(List<String> userInfo){
+
+    }
+
+    private static void addFunds(List<String> userInfo){
+
+    }
+
+    private static void endDay(){
+
+    }
+
+    private static void monthlyExpenditue(){
+
+    }
+}

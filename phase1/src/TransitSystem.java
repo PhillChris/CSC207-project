6be4dd2ba5173804b8960f647a7ce5a556174c4d
Fardin Syed <fardin.syed99@gmail.com>
2018-07-05 20:49:57 -0400

    Changed type from ArrayList to List in TransitSystem

--- phase1/src/TransitSystem.java
+++ phase1/src/TransitSystem.java
@@ -5,66 +5,64 @@ import java.time.LocalDate;
 
 /** This wraps together all the contents of TransitSystem */
 public class TransitSystem {
-  private static HashMap<String, ArrayList<Station>> subwayRoutes;
-  private static HashMap<String, ArrayList<Station>> busRoutes;
+  private static HashMap<String, List<Station>> subwayRoutes;
+  private static HashMap<String, List<Station>> busRoutes;
   private static List<Trip> allTrip;
   private static List<CardHolder> allUsers;
-  private static int month;
-  private static int day;
   LocalDate currentTime;
 
   public static double getTotalRevenues() {
     return 0;
   }
 
-  public static HashMap<String, ArrayList<Station>> getBusRoutes() {
+  public static HashMap<String, List<Station>> getBusRoutes() {
     return busRoutes;
   }
 
-  public static HashMap<String, ArrayList<Station>> getSubwayRoutes() {
+  public static HashMap<String, List<Station>> getSubwayRoutes() {
     return subwayRoutes;
   }
 
   public static List<CardHolder> getUsers() {return allUsers; }
 
-  public static void setBusRoutes(HashMap<String, ArrayList<Station>> newroutes) {
+  public static void setBusRoutes(HashMap<String, List<Station>> newroutes) {
     busRoutes = newroutes;
   }
 
-  public static void setSubwayRoutes(HashMap<String, ArrayList<Station>> newroutes) {
+  public static void setSubwayRoutes(HashMap<String, List<Station>> newroutes) {
     subwayRoutes = newroutes;
   }
 
-  public static void addBusRoute(String name, ArrayList<String> routes){
+  public static void addBusRoute(String name, List<String> routes){
     subwayRoutes.put(name, new ArrayList<Station>());
 
   }
-  public static void addSubwayRoute(String name, ArrayList<String> routes){
+  public static void addSubwayRoute(String name, List<String> routes){
     subwayRoutes.put(name, new ArrayList<Station>());
 
   }
 
-  public static void tap(ArrayList<String> cardInfo){
+  public static void tap(List<String> cardInfo){
 
   }
 
-  public static void addUser(ArrayList<String> userInfo){
+  public static void addUser(List<String> userInfo){
 
   }
 
-  public static void addCard(ArrayList<String> cardInfo){
+  public static void addCard(List<String> cardInfo){
 
   }
 
-  public static void removeCard(ArrayList<String> cardInfo){
+  public static void removeCard(List<String> cardInfo){
 
   }
 
-  public static void reportTheft(ArrayList<String> userInfo){
+  public static void reportTheft(List<String> userInfo){
 
   }
 
-  public static void addFunds(ArrayList<String> userInfo){
+  public static void addFunds(List<String> userInfo){
 
   }
 

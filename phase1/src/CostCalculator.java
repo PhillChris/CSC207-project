c150f9b57104f7332dbb14b17861d6dcdb432acd
Fardin Syed <fardin.syed99@gmail.com>
2018-07-09 00:34:16 -0400

    Changed updateUserRevenue to loop through all users

--- phase1/src/CostCalculator.java
+++ phase1/src/CostCalculator.java
@@ -21,33 +21,38 @@ public class CostCalculator {
    * @param cards
    * @return Hashmap with lists of trip costs for a given month/year
    */
-  public static void updateUserRevenue(CardHolder user) {
+  public static void updateUserRevenue() {
     // Get the current date and month
     LocalDate date = TransitTime.getCurrentDate();
     YearMonth month = YearMonth.of(date.getYear(), date.getMonth());
 
-    List<Card> cards = user.getCards(); // Users cards
-    int monthlyCost = 0;
-    int dailyCost = 0;
+    // Loop through all the Users in the System
+    for (String userName : CardHolder.getAllUsers().keySet()) {
+      CardHolder user = CardHolder.getAllUsers().get(userName);
 
-    // Loop through all cards
-    for (Card card : cards) {
-      for (Trip trip : card.getAllTrips()) {
-        LocalDateTime tripStart = trip.getTimeStarted();
-        YearMonth tripMonthYear = YearMonth.of(tripStart.getYear(), tripStart.getMonth());
-        // If the Trip occured in the current month
-        if (month.equals(tripMonthYear)) {
-          monthlyCost += trip.getFee();
-        }
-        // If the Trip occured in the current day
-        if (date.equals(tripStart)){
-          dailyCost += trip.getFee();
+      List<Card> cards = user.getCards(); // Users' cards
+      int monthlyCost = 0;
+      int dailyCost = 0;
+
+      // Loop through all cards
+      for (Card card : cards) {
+        for (Trip trip : card.getAllTrips()) {
+          LocalDateTime tripStart = trip.getTimeStarted();
+          YearMonth tripMonthYear = YearMonth.of(tripStart.getYear(), tripStart.getMonth());
+          // If the Trip occured in the current month
+          if (month.equals(tripMonthYear)) {
+            monthlyCost += trip.getFee();
+          }
+          // If the Trip occured in the current day
+          if (date.equals(tripStart)) {
+            dailyCost += trip.getFee();
+          }
         }
       }
+      // Update User's HashMap
+      user.addMonthlyFee(monthlyCost);
+      user.addDailyFee(dailyCost);
     }
-    // Update User's HashMap
-    user.addMonthlyFee(monthlyCost);
-    user.addDailyFee(dailyCost);
   }
 
 

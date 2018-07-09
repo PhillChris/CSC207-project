150bd12144e6bca537824ff042127adc90e8a50c
ren <rzhang0701@gmail.com>
2018-07-09 18:50:33 -0400

    Changed AdminUser.dailyReports to append new lines rather than rewrite itself each time.

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -20,29 +20,20 @@ public class AdminUser extends CardHolder {
   }
 
   public static void dailyReports() {
-    // The revenue collected for each day
-    HashMap<LocalDate, Integer> dailyTotals = CostCalculator.getDailyRevenue();
-    // List of all the dates collected
-    List<LocalDate> dates = new ArrayList<LocalDate>(dailyTotals.keySet());
-    Collections.sort(dates);
-
     try {
       // Writes to dailyReports.txt
       Writer writer =
           new BufferedWriter(
-              new OutputStreamWriter(new FileOutputStream("dailyReports.txt"), "utf-8"));
+              new FileWriter("dailyReports.txt", true));
       writer.write("Date       Revenue");
       writer.write(System.lineSeparator());
 
-      // Loop through all days and write each day's revenue
-      for (LocalDate day : dates) {
-        String date = day.toString();
-        double total = dailyTotals.get(day) / 100;
-        String revenue = String.format("%.2f", total);
-        writer.write(String.format("%s", date + " $" + revenue));
-      }
+      // Append the new day's revenue
 
-      writer.close();
+      LocalDate date = TransitTime.currentTime.toLocalDate();
+      double total = CostCalculator.DailyRevenue.get(date) / 100;
+      String revenue = String.format("%.2f", total);
+      writer.write(String.format("%s", date + " $" + revenue));
 
     } catch (IOException e) {
       System.out.println("File could not be constructed. Daily reports will not be generated.");

b2c43666b467f54e6933b57170a2bb47c4dc6904
Fardin Syed <fardin.syed99@gmail.com>
2018-07-09 00:08:01 -0400

    Updated updateUserCosts to update DailyCosts

--- phase1/src/CostCalculator.java
+++ phase1/src/CostCalculator.java
@@ -22,22 +22,32 @@ public class CostCalculator {
    * @return Hashmap with lists of trip costs for a given month/year
    */
   public static void updateUserCosts(CardHolder user) {
+    // Get the current date and month
     LocalDate date = TransitTime.getCurrentDate();
     YearMonth month = YearMonth.of(date.getYear(), date.getMonth());
-    List<Card> cards = user.getCards();
+
+    List<Card> cards = user.getCards(); // Users cards
     int monthlyCost = 0;
+    int dailyCost = 0;
+
     // Loop through all cards
     for (Card card : cards) {
       for (Trip trip : card.getAllTrips()) {
         LocalDateTime tripStart = trip.getTimeStarted();
         YearMonth tripMonthYear = YearMonth.of(tripStart.getYear(), tripStart.getMonth());
+        // If the Trip occured in the current month
         if (month.equals(tripMonthYear)) {
           monthlyCost += trip.getFee();
+        }
+        // If the Trip occured in the current day
+        if (date.equals(tripStart)){
+          dailyCost += trip.getFee();
         }
       }
     }
     // Update User's HashMap
     user.addMonthlyFee(monthlyCost);
+    user.addDailyFee(dailyCost);
   }
 
   /**

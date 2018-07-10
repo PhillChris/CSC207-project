4d3ebf1c3a94c7ca7677f132ea03643b80e149de
Fardin Syed <fardin.syed99@gmail.com>
2018-07-10 01:53:18 -0400

    Added documentation

--- phase1/src/BusFactory.java
+++ phase1/src/BusFactory.java
@@ -1,3 +1,4 @@
+/** A Factory for constructing BusStations */
 public class BusFactory implements StationFactory {
   @Override
   public Station newStation(String name) {
--- phase1/src/BusStation.java
+++ phase1/src/BusStation.java
@@ -1,8 +1,11 @@
 import java.util.HashMap;
 
 public class BusStation extends Station {
+  /** A HashMap linking BusStation names to BusStations */
   public static HashMap<String, Station> busStations = new HashMap<>();
+  /** The initial fee charged by starting a trip at a BusStation */
   private int BUS_INITIAL_FEE = 200;
+  /** The fee charged per BusStation travelled */
   private int BUS_PERSTATION_FEE = 0;
 
   /**
@@ -16,14 +19,29 @@ public class BusStation extends Station {
     this.perStationFee = BUS_PERSTATION_FEE;
   }
 
+  /**
+   * Adds this station to the HashMap busStations
+   *
+   * @param station The station to be added
+   */
   public static void newBusStation(Station station) {
     busStations.put(station.getName(), station);
   }
 
+  /**
+   * Get all the BusStations from a static context
+   *
+   * @return The HashMap of BusStations
+   */
   public static HashMap<String, Station> getBusStations() {
     return busStations;
   }
 
+  /**
+   * Get all the BusStations from a non static context
+   *
+   * @return The HashMap of BusStations
+   */
   public HashMap<String, Station> getStations() {
     return busStations;
   }

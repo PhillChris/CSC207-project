cae924946a9134bbb296f980235ce4b22d908499
Andre Daprato <andre.daprato5@gmail.com>
2018-07-09 23:56:35 -0400

    uncommented BusRoute Code

--- phase1/src/BusRoute.java
+++ phase1/src/BusRoute.java
@@ -1,28 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BusRoute {
+public class BusRoute extends Route {
 
-//  /** An ArrayList of all the BusRoutes in the simulation */
-//  private static List<BusRoute> busRoutes = new ArrayList<>();
-//
-//  public BusRoute(List<String> stationNames) {
-//    super();
-//
-//    // Add routeStations to this Route
-//    for (String s : stationNames) {
-//      BusStation station;
-//      if (!BusStation.getStations().containsKey(s)) { // there are no stations with the same name.
-//        station = new BusStation(s);
-//        Station.addStation(station); // associate stations
-//        BusStation.addStation(station);
-//      } else {
-//        station = BusStation.getStations().get(s);
-//        }
-//      getRouteStations().add(station);
-//    }
-//    // Add itself to the ArrayLists of routeStations
-//    busRoutes.add(this);
-//    Routes.add(this);
-//  }
+  /** An ArrayList of all the BusRoutes in the simulation */
+  private static List<BusRoute> busRoutes = new ArrayList<>();
+
+  public BusRoute(List<String> stationNames) {
+    super();
+
+    // Add routeStations to this Route
+    for (String s : stationNames) {
+      BusStation station;
+      if (!BusStation.getStations().containsKey(s)) { // there are no stations with the same name.
+        station = new BusStation(s);
+        Station.addStation(station); // associate stations
+        BusStation.addStation(station);
+      } else {
+        station = BusStation.getStations().get(s);
+        }
+      getRouteStations().add(station);
+    }
+    // Add itself to the ArrayLists of routeStations
+    busRoutes.add(this);
+    Routes.add(this);
+  }
 }

2f0d9c55a910e259c24d866a2dea76526950e5f4
Andre Daprato <andre.daprato5@gmail.com>
2018-07-21 17:05:14 -0400

    added empty AdminUserPage extending Page.

---
+++ phase2/src/AdminUserPage.java
@@ -0,0 +1,13 @@
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminUserPage extends Page {
+
+  public AdminUserPage(Stage primaryStage, UserParser userParser, CardParser cardParser) {
+    super(primaryStage, userParser, cardParser);
+  }
+  @Override
+  Scene makeScene(Stage primaryStage) {
+    return null;
+  }
+}

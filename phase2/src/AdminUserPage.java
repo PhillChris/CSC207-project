75478eb90da3ce0e98eedf7574027d3d17253129
Andre Daprato <andre.daprato5@gmail.com>
2018-07-22 06:24:05 -0400

    implemented part of makeScene in AdminUserPage.

--- phase2/src/AdminUserPage.java
+++ phase2/src/AdminUserPage.java
@@ -1,4 +1,6 @@
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AdminUserPage extends AuthenticatedPage {
@@ -14,10 +16,32 @@ public class AdminUserPage extends AuthenticatedPage {
 
   @Override
   Scene makeScene(Stage primaryStage) {
+    placeLabel("Station name: ", 0, 1);
+    TextField stationName = placeTextField(1, 1);
+
+    placeLabel("Station type: ", 0, 2);
+    ChoiceBox<String> stationTypes = new ChoiceBox<>();
+    stationTypes.getItems().addAll("Bus", "Subway");
+    stationTypes.getSelectionModel().select(0);
+
+    grid.add(stationTypes, 1, 2);
+    placeButton(
+            "Make Station", () -> makeNewStation(stationName.getText(), stationTypes.getValue()), 1, 3);
+
     return new Scene(grid, 300, 250);
   }
 
   public void updatePage(Stage primaryStage) {}
 
   public void addUserData(Stage primaryStage) {}
+
+  private void makeNewStation(String name, String type) {
+    if (type.equals("Bus")) {
+      new BusStation(name);
+      System.out.println("New bus");
+    } else {
+      new SubwayStation(name);
+      System.out.println("New subway");
+    }
+  }
 }

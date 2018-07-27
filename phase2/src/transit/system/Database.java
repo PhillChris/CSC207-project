3542e8e0baa5dadfbda4e4a957d72b90ebbbe5b5
Andre Daprato <andre.daprato5@gmail.com>
2018-07-27 10:18:40 -0400

    implemented part of writeToDatabase method in Database class.

--- phase2/src/transit/system/Database.java
+++ phase2/src/transit/system/Database.java
@@ -1,4 +1,26 @@
 package transit.system;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Database {
+  public static final String routeLocation = "/tmp/routes.ser";
+
+  public static void writeToDatabase() {
+    writeRoutes();
+  }
+
+  private static void writeRoutes() {
+    HashMap<String, ArrayList<Route>> routes = Route.getRoutesCopy();
+    try {
+      FileOutputStream fileOut = new FileOutputStream(routeLocation);
+      ObjectOutputStream out = new ObjectOutputStream(fileOut);
+      out.writeObject(routes);
+    } catch (IOException e) {
+      System.out.println("Couldn't find file");
+    }
+  }
 }

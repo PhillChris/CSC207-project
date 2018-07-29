b4216551e7936db7c71a8a3242148a0cd2970515
Fardin Syed <fardin.syed99@gmail.com>
2018-07-28 23:37:28 -0400

    Reformatted AddRoutePage with different buttons

--- phase2/src/transit/pages/RouteCreationPage.java
+++ phase2/src/transit/pages/RouteCreationPage.java
@@ -2,11 +2,13 @@ package transit.pages;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import org.controlsfx.control.CheckComboBox;
@@ -14,6 +16,7 @@ import transit.system.Route;
 import transit.system.Station;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /** Represents a page used to make maintenance changes to this system */
 public class RouteCreationPage extends Page {
@@ -28,85 +31,29 @@ public class RouteCreationPage extends Page {
 
   @Override
   void makeScene(Stage primaryStage) {
-    placeButton("Add route", () -> createRoutePage(), 10, 10);
-  }
-
-  private void createRoutePage() {
-    Stage secondaryStage = new Stage();
-
-  }
-
-  /** Constructs the UI for creating a Station. */
-  private void makeStationConstruction() {
-    placeLabel("Station name: ", 0, 1);
-    TextField stationName = placeTextField(1, 1);
-
-    placeLabel("Station type: ", 0, 2);
-    ChoiceBox<String> stationTypes = new ChoiceBox<>();
-    stationTypes.getItems().addAll("Bus", "Subway");
-    stationTypes.getSelectionModel().select(0);
-
-    grid.add(stationTypes, 1, 2);
-    placeButton(
-        "Make Station",
-        () -> {
-          try {
-            String stationType = stationTypes.getValue();
-            Station newStation = new Station(stationName.getText(), stationType);
-            if (routeType.getValue().equals(stationType)) {
-              this.dropDownList.getItems().add(newStation);
-            }
-          } catch (Exception e) {
-            makeAlert(
-                    "Station Construction",
-                    "Station name",
-                    "A station of the same type and name already exists.",
-                    Alert.AlertType.WARNING)
-                .showAndWait();
-          }
-        },
-        1,
-        3);
-  }
-
-  /**
-   * Constructs the UI for making routes from Stations of the given type.
-   *
-   * @param type the type of Stations to display in the dropDownList.
-   * @return
-   */
-  private void makeRouteConstruction(String type) {
-    ObservableList<Station> stations;
-    try {
-      stations = FXCollections.observableList(Station.getStations(type));
-    } catch (Exception e) {
-      stations = FXCollections.observableList(new ArrayList<>());
+    grid.setPadding(new Insets(30, 20, 20, 40));
+    grid.setHgap(10);
+    grid.setVgap(10);
+    placeButton("Add subway route", () -> createSubwayRoute(), 0, 0);
+    placeButton("Add bus route", () -> createBusRoute(), 0, 2);
+    placeLabel("Append to Existing Station:", 0, 4);
+    for (int i = 0; i < Route.getRoutesCopy().size(); i++){
+      placeButton(Route.getRoutesCopy().get(i).toString(), () -> appendExistingRoute(), 0, 6 + 2*i);
     }
-    ObservableList<Station> oldStations = this.dropDownList.getItems();
-    oldStations.remove(0, oldStations.size()); // clear the old stations from view
-    this.dropDownList.getCheckModel().clearChecks();
-    // add to the drop down list all the stations of given type.
-    this.dropDownList.getItems().addAll(stations);
-    // the button that constructs the routes from the stations selected in the dropDownList.
-    grid.getChildren().remove(this.dropDownList);
-    placeLabel("Select Stations: ", 0, 7);
-    grid.add(this.dropDownList, 1, 7);
-    placeButton(
-        "Make Route",
-        () -> {
-          try {
-            new Route(dropDownList.getCheckModel().getCheckedItems(), routeType.getValue());
-            this.dropDownList.getCheckModel().clearChecks();
-          } catch (Exception a) {
-            makeAlert(
-                    "Invalid Station Type",
-                    "Invalid Station Type",
-                    "The station type you are entering is not a station type recognized by the system",
-                    AlertType.ERROR)
-                .showAndWait();
-          }
-        },
-        1,
-        8);
+
+    this.scene = new Scene(grid, 600, 400);
+
+  }
+
+  private void createSubwayRoute(){
+
+  }
+
+  private void createBusRoute(){
+
+  }
+
+  private void appendExistingRoute(){
+
   }
 }

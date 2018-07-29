fea7736e32d5fd1a51fc7d68ce824152a66d55f0
Fardin Syed <fardin.syed99@gmail.com>
2018-07-29 14:56:26 -0400

    Merged branches

---
+++ phase2/src/transit/system/UserStatistics.java
@@ -0,0 +1,9 @@
+package transit.system;
+
+/** This class is designed to track all statistics related to a user */
+public class UserStatistics {
+  /** The user assoicated with these statistics */
+  User user;
+
+
+}

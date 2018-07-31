cf25f1b568aef4bfdfa646c47b59ea7d2dc0520a
ren <rzhang0701@gmail.com>
2018-07-31 15:55:03 -0400

    Initial styling of SignUpPage

--- phase2/src/transit/pages/SignUpPage.java
+++ phase2/src/transit/pages/SignUpPage.java
@@ -1,15 +1,20 @@
 package transit.pages;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import transit.system.AdminUser;
 import transit.system.User;
+
+import javax.xml.soap.Text;
 
 /** Represents a page opened when making a new account in this transit system */
 public class SignUpPage extends Page {
@@ -30,21 +35,86 @@ public class SignUpPage extends Page {
    */
   @Override
   protected void makeScene(Stage primaryStage) {
-    placeLabels();
+    grid.setPadding(new Insets(20, 20, 20, 40));
+    grid.setHgap(10);
+    grid.setVgap(10);
 
-    TextField username = placeTextField(1, 1);
-    TextField email = placeTextField(1, 2);
-    PasswordField password = placePasswordField(1, 3);
-    CheckBox adminBox = placeCheckBox("Is Admin?", 1, 4);
+    GridPane signupPane = new GridPane();
+    signupPane.setHgap(10);
+    signupPane.setVgap(12);
 
-    makeButtons(
-        primaryStage,
-        username,
-        email,
-        password,
-        adminBox);
+    Label signup = new Label("Sign up");
+    signupPane.add(signup, 0, 0, 2, 1);
+    signup.setId("signUpLabel");
 
-    this.scene = new Scene(grid, Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+    ImageView faceIcon = new ImageView(new Image("transit/pages/assets/face.png"));
+    signupPane.add(faceIcon, 0, 1);
+
+    ImageView emailIcon = new ImageView(new Image("transit/pages/assets/email.png"));
+    signupPane.add(emailIcon, 0, 2);
+
+    ImageView keyIcon = new ImageView(new Image("transit/pages/assets/key.png"));
+    signupPane.add(keyIcon, 0, 3);
+
+    TextField userInput = new TextField();
+    userInput.setPromptText("Username");
+    signupPane.add(userInput, 1, 1);
+
+    TextField emailInput = new TextField();
+    emailInput.setPromptText("Email");
+    signupPane.add(emailInput, 1, 2);
+
+
+    PasswordField passInput = new PasswordField();
+    passInput.setPromptText("Password");
+    signupPane.add(passInput, 1, 3);
+
+
+    CheckBox adminBox = new CheckBox("Admin?");
+    signupPane.add(adminBox, 0, 4, 2, 1);
+
+    Label errorMessage = new Label("");
+    errorMessage.setId("errorMessage");
+    signupPane.add(errorMessage, 1, 4);
+    GridPane.setHalignment(errorMessage, HPos.RIGHT);
+
+    Separator horizontalSeparator = new Separator();
+    signupPane.add(horizontalSeparator, 0, 5, 2, 1);
+
+    Button signUpButton = new Button("Sign Up");
+    signUpButton.setOnAction((data) -> {
+      try {
+        add(userInput.getText(), emailInput.getText(), passInput.getText(), adminBox.isSelected());
+        errorMessage.setText("Account created");
+        errorMessage.setTextFill(Color.web("#33AF54"));
+      } catch (Exception ignored) {
+        errorMessage.setTextFill(Color.web("red"));
+        errorMessage.setText("Email invalid or in use");
+      }
+    });
+    signupPane.add(signUpButton, 0, 6, 2, 1);
+    GridPane.setHalignment(signUpButton, HPos.LEFT);
+    GridPane.setMargin(signUpButton, new Insets(0, 0, 0, 0));
+
+
+
+    Button backButton = new Button("Go Back");
+    backButton.setOnAction((data) -> primaryStage.setScene(new LoginPage(primaryStage).getScene()));
+    signupPane.add(backButton, 1, 6, 2, 1);
+    GridPane.setHalignment(backButton, HPos.RIGHT);
+    GridPane.setMargin(backButton, new Insets(0, 0, 0, 0));
+
+    grid.add(signupPane, 0, 1);
+
+    GridPane trainPane = new GridPane();
+    ImageView train = new ImageView(new Image("transit/pages/assets/train.png"));
+    trainPane.setPadding(new Insets(30, 0, 0, 10));
+    trainPane.add(train, 0, 0);
+    grid.add(trainPane, 1, 0, 1, 2);
+
+    addClock();
+    this.scene = new Scene(grid, 625, 400);
+    scene.getStylesheets().add(SignUpPage.class.getResource("styling/SignUpPage.css").toExternalForm());
   }
 
   /**
@@ -76,10 +146,11 @@ public class SignUpPage extends Page {
    * @param username the username of the user which is being created
    * @param email the email of the user which is being created
    * @param password the password of the user which is being created
-   * @param isAdmin whether or not the user being created is an admin
+   * @param adminBox whether or not the user being created is an admin
    */
   private void makeButtons(
-      Stage primaryStage, TextField username, TextField email, PasswordField password, CheckBox adminBox) {
+      Stage primaryStage, TextField username, TextField email, PasswordField password,
+      CheckBox adminBox, Label errorMessage) {
     placeButton(
         "Go back", () -> primaryStage.setScene(new LoginPage(primaryStage).getScene()), 1, 5);
 
@@ -90,14 +161,15 @@ public class SignUpPage extends Page {
             add(username.getText(), email.getText(), password.getText(), adminBox.isSelected());
             primaryStage.setScene(new LoginPage(primaryStage).getScene());
           } catch (Exception a) {
-            Alert alert =
-                makeAlert(
-                    "Email in use!",
-                    "Email in use:",
-                    "The email you provided is not valid or"
-                        + " is currently in use by another user.",
-                    AlertType.WARNING);
-            alert.showAndWait();
+//            Alert alert =
+//                makeAlert(
+//                    "Email in use!",
+//                    "Email in use:",
+//                    "The email you provided is not valid or"
+//                        + " is currently in use by another user.",
+//                    AlertType.WARNING);
+//            alert.showAndWait();
+
           }
         },
         1,

b7d9c14690144af4b8dc331ed1e0702b27fff003
Andre Daprato <andre.daprato5@gmail.com>
2018-07-31 19:49:40 -0400

    added Statistics interface.

---
+++ phase2/src/transit/system/Statistics.java
@@ -0,0 +1,38 @@
+package transit.system;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.HashMap;
+
+public interface Statistics<T> {
+
+  /**
+   * Generates statistics for each day in the past week (the last 7 days)
+   *
+   * @return a HashMap of the statistics on each day of the past week
+   */
+  HashMap<LocalDate, T> generateWeeklyMessage();
+
+  /**
+   * Generates statistics for each month in the past year (the last 7 days)
+   *
+   * @return a HashMap of the statistics on each day of the past week
+   */
+  HashMap<YearMonth, T> generateMonthlyMessage();
+
+  /**
+   * Create default values for the HashMap returned by the generateWeeklyMessage method. This makes
+   * the interface work easily when a week has not yet passed in the application.
+   *
+   * @return a HashMap containing default (dummy) values.
+   */
+  HashMap<LocalDate, T> defaultWeeklyValues();
+
+  /**
+   * Create default values for the HashMap returned by the generateMonthlyMessage method. This makes
+   * the interface work easily when a year has not yet passed in the application.
+   *
+   * @return a HashMap containing default (dummy) values.
+   */
+  HashMap<YearMonth, T> defaultMonthlyValues();
+}

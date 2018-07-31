b2d6c329267b49a85c5430b4b34838c4c715385b
Andre Daprato <andre.daprato5@gmail.com>
2018-07-31 12:13:57 -0400

    made LoginPage use non-default constructor from Page.

--- phase2/src/transit/pages/LoginPage.java
+++ phase2/src/transit/pages/LoginPage.java
@@ -3,11 +3,7 @@ package transit.pages;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -24,6 +20,7 @@ public class LoginPage extends Page {
    * @param primaryStage The stage for this page to be displayed
    */
   public LoginPage(Stage primaryStage) {
+    super(primaryStage);
     makeScene(primaryStage);
   }
 

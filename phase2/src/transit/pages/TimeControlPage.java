1acc0555883311382d44d5796deae02604800ade
PhillChris <chrph@utschools.ca>
2018-08-02 10:23:11 -0400

    Minor styling of time control window

--- phase2/src/transit/pages/TimeControlPage.java
+++ phase2/src/transit/pages/TimeControlPage.java
@@ -1,28 +1,35 @@
 package transit.pages;
 
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 import transit.system.TransitTime;
 
-/** Represents a time control window in this simulation*/
+/** Represents a time control window in this simulation */
 public class TimeControlPage extends Page {
-  /** Contructs a new instance of the */
+  /** Constructs a new instance of TimeControlPage */
   public TimeControlPage(Stage secondaryStage) {
     makeScene(secondaryStage);
   }
 
+  /**
+   * Builds the contents of the TimeControlPage
+   *
+   * @param secondaryStage the alternate window on which this popup is served
+   */
   void makeScene(Stage secondaryStage) {
-    placeButton("Pause time", () -> TransitTime.pauseTime(), 0, 15);
+    placeButton("Pause time", () -> TransitTime.pauseTime(), 0, 0);
 
-    placeButton("Start time", () -> TransitTime.startTime(), 0, 16);
+    placeButton("Start time", () -> TransitTime.startTime(), 0, 1);
 
-    placeButton("Jump ahead 1 hour", () -> TransitTime.fastForward(), 0, 17);
+    placeButton("Jump ahead 1 hour", () -> TransitTime.fastForward(), 0, 2);
 
-    placeButton("Jump ahead 1 day", () -> TransitTime.skipDay(), 0, 18);
+    placeButton("Jump ahead 1 day", () -> TransitTime.skipDay(), 0, 3);
 
-    placeButton("Jump ahead 1 month", () -> TransitTime.skipMonth(), 0, 19);
+    placeButton("Jump ahead 1 month", () -> TransitTime.skipMonth(), 0, 4);
 
-    this.scene = new Scene(grid, Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+    grid.setAlignment(Pos.CENTER);
+
+    this.scene = new Scene(grid, 150, 150);
   }
 }

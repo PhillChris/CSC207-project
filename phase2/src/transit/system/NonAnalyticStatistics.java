4d5575637eb569a10021e60497846e73363db3a2
Fardin Syed <fardin.syed99@gmail.com>
2018-08-04 00:03:37 -0400

    Made nonAnalyticStatistics Serializable

--- phase2/src/transit/system/NonAnalyticStatistics.java
+++ phase2/src/transit/system/NonAnalyticStatistics.java
@@ -1,12 +1,13 @@
 package transit.system;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /** Records non analytic statistics associated with an object */
-public class NonAnalyticStatistics<T> implements Statistics<T> {
+public class NonAnalyticStatistics<T> implements Statistics<T>, Serializable {
 
     private HashMap<LocalDate, List<T>> dailyLogs;
 

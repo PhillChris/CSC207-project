8894e743f8eda535457b74cb186b1e024c934c95
Fardin Syed <fardin.syed99@gmail.com>
2018-08-04 13:40:27 -0400

    Stored fee types in a HashMap

--- phase2/src/transit/system/Station.java
+++ phase2/src/transit/system/Station.java
@@ -16,20 +16,10 @@ public class Station implements Serializable {
   private static final int SUBWAY_PERSTATION_FEE = 50;
   /** The fee charged when passing each subway station to students */
   private static final int STUDENT_SUBWAY_PERSTATION_FEE = 40;
-  /** The fee charged per station travelled by this station */
-  private int perStationFee;
-  /** The initial fee charged by this station at the start of a trip */
-  private int initialFee;
-  /** The fee charged per station travelled by students by this station */
-  private int perStationStudentFee;
-  /** The initial fee charged by this station to students at the start of a trip*/
-  private int initialStudentFee;
+  /** The fees associated with this station */
+  private HashMap<String, Integer> fees = new HashMap<>();
   /** The name of this station */
   private String name;
-  /** The number of people who have tapped on at this station per day. */
-  private HashMap<LocalDate, Integer> tapsOn = new HashMap<>();
-  /** The number of people who have tapped off at this station per day. */
-  private HashMap<LocalDate, Integer> tapsOff = new HashMap<>();
   /** The stationType of this station */
   private String stationType;
   /** Stores associated statistics to this user */
@@ -62,17 +52,17 @@ public class Station implements Serializable {
   /** @return the fare charged by this station at the start of a new trip portion */
   public int getInitialFee(String permission) {
     if (permission == "student") {
-      return this.initialStudentFee;
+      return fees.get("initialStudentFee");
     }
-    return this.initialFee;
+    return fees.get("initialFee");
   }
 
   /** @return the fare charged by this station when a user travels by it */
   public int getPerStationFee(String permission) {
     if (permission == "student") {
-      return perStationStudentFee;
+      return fees.get("perStationStudentFee");
     }
-    return perStationFee;
+    return fees.get("perStationFee");
   }
 
   /** Records the tapping in of a station */
@@ -80,20 +70,18 @@ public class Station implements Serializable {
     statistics.get(statName).update(quantity);
   }
 
-  /**
-   * Sets the fees of this station
-   */
+  /** Sets the fees of this station */
   private void setFees() {
     if (stationType.equals("Bus")) {
-      this.perStationFee = 0;
-      this.initialFee = BUS_INITIAL_FEE;
-      this.perStationStudentFee = 0;
-      this.initialStudentFee = STUDENT_BUS_INITIAL_FEE;
+      fees.put("perStationFee", 0);
+      fees.put("initialFee", BUS_INITIAL_FEE);
+      fees.put("perStationStudentFee", 0);
+      fees.put("initialStudentFee", STUDENT_BUS_INITIAL_FEE);
     } else {
-      this.perStationFee = SUBWAY_PERSTATION_FEE;
-      this.initialFee = 0;
-      this.perStationStudentFee = STUDENT_SUBWAY_PERSTATION_FEE;
-      this.initialStudentFee = 0;
+      fees.put("perStationFee", SUBWAY_PERSTATION_FEE);
+      fees.put("initialFee", 0);
+      fees.put("perStationStudentFee", STUDENT_SUBWAY_PERSTATION_FEE);
+      fees.put("initialStudentFee", 0);
     }
   }
 }

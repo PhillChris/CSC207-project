a03842f25419017d308cb99d4f0211df300d2f9e
Fardin Syed <fardin.syed99@gmail.com>
2018-08-03 22:41:50 -0400

    Moved UserRevenue methods to Abstract Super class

--- phase2/src/transit/system/Statistics.java
+++ phase2/src/transit/system/Statistics.java
@@ -5,35 +5,65 @@ import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class Statistics<T extends Number> {
+public abstract class Statistics {
   /** Log of the daily values stored by this statistic */
-  HashMap<LocalDate, ArrayList<T>> dailyLogs;
+  HashMap<LocalDate, Double> dailyLogs;
 
   /**
    * Generates a string representation of statistics for each day in the past week (the last 7 days)
    *
    * @return a HashMap of the statistics on each day of the past week
    */
-  abstract HashMap<LocalDate, T> generateWeeklyValues();
+  HashMap<LocalDate, Double> generateWeeklyValues(){
+    HashMap<LocalDate, Double> expenditures = new HashMap<>();
+    LocalDate date = TransitTime.getCurrentDate();
+    // Loop through the last seven days
+    while (!date.equals(date.minusDays(7))) {
+      expenditures.put(date, dailyLogs.get(date));
+      date = date.minusDays(1);
+    }
+    return expenditures;
+  }
 
   /**
    * Generates a string representation of statistics for each month in the past year (the last 7 days)
    *
    * @return a HashMap of the statistics on each day of the past week
    */
-  abstract HashMap<YearMonth, T> generateMonthlyValues();
+  HashMap<YearMonth, Double> generateMonthlyValues(){
+    HashMap<YearMonth, Double> expenditures = new HashMap<>();
+    YearMonth month = TransitTime.getCurrentMonth();
+    // Loop through the past year
+    while (!month.equals(month.minusMonths(12))) {
+      double cost = calculateMonthlyCost(month);
+      expenditures.put(month, cost);
+      month = month.minusMonths(1);
+    }
+    return expenditures;
+  }
 
   /** Throws out old statistics */
   void clearLogs() {}
 
   /** Updates the information stored by this statistic */
-  void update(T data) {
+  void update(Double data) {
     LocalDate date = TransitTime.getCurrentDate();
     // Update user's personal monthly expenditure History
     if (!dailyLogs.containsKey(date)) {
-      dailyLogs.put(date, new ArrayList<T>());
-      dailyLogs.get(date).add(data);
+      dailyLogs.put(date, data);
     }
-    dailyLogs.get(date).add(data);
+    else{
+      dailyLogs.put(date, dailyLogs.get(date) + data);
+    }
+  }
+
+  protected double calculateMonthlyCost(YearMonth month) {
+    double sum = 0.0;
+    LocalDate date = month.atEndOfMonth();
+    while (date != month.minusMonths(1).atEndOfMonth()) {
+      Double value = dailyLogs.get(date);
+      date = date.minusDays(1);
+    }
+    return sum;
   }
 }
--- phase2/src/transit/system/UserRevenue.java
+++ phase2/src/transit/system/UserRevenue.java
@@ -6,69 +6,11 @@ import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class UserRevenue extends Statistics<Double> implements Serializable {
+public class UserRevenue extends Statistics implements Serializable {
 
   /** Instantiates a new instance of UserRevenue */
   public UserRevenue() {
-    dailyLogs = new HashMap<LocalDate, ArrayList<Double>>();
+    dailyLogs = new HashMap<LocalDate, Double>();
   }
 
-  /** @return A String detailing average expenditure per month of this transit.system.User. */
-  String avgMonthlyMessage() {
-    String message = "Cost per month for user: ";
-    return message.trim();
-  }
-
-  @Override
-  public HashMap<LocalDate, Double> generateWeeklyValues() {
-    HashMap<LocalDate, Double> expenditures = new HashMap<>();
-    LocalDate date = TransitTime.getCurrentDate();
-    // Loop through the last seven days
-    while (!date.equals(date.minusDays(7))) {
-      double cost = calculateDayCost(date);
-      expenditures.put(date, cost);
-      date = date.minusDays(1);
-    }
-    return expenditures;
-  }
-
-  @Override
-  public HashMap<YearMonth, Double> generateMonthlyValues() {
-    HashMap<YearMonth, Double> expenditures = new HashMap<>();
-    YearMonth month = TransitTime.getCurrentMonth();
-    // Loop through the past year
-    while (!month.equals(month.minusMonths(12))) {
-      double cost = calculateMonthlyCost(month);
-      expenditures.put(month, cost);
-      month = month.minusMonths(1);
-    }
-    return expenditures;
-  }
-
-  /**
-   * A private helper to calculate the cost of the system for a given month
-   *
-   * @param month A given month in the calendar year
-   * @return The total expenditure logged for the given month
-   */
-  private double calculateMonthlyCost(YearMonth month) {
-    double sum = 0.0;
-    LocalDate date = month.atEndOfMonth();
-    while (date != month.minusMonths(1).atEndOfMonth()) {
-      Double value = calculateDayCost(date);
-      date = date.minusDays(1);
-    }
-    return sum;
-  }
-
-  private double calculateDayCost(LocalDate date) {
-    double sum = 0.0;
-    ArrayList<Double> values = dailyLogs.get(date);
-    if (values != null) {
-      for (double value : values) {
-        sum += value;
-      }
-    }
-    return sum;
-  }
 }

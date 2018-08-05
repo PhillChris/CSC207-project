6d83eb8f79f35f670b64f187720bbc0fdf5355be
Andre Daprato <andre.daprato5@gmail.com>
2018-08-04 20:13:39 -0400

    created rough implemntation of statistical graphs.

--- phase2/src/transit/pages/AdminGraphPage.java
+++ phase2/src/transit/pages/AdminGraphPage.java
@@ -1,23 +1,20 @@
 package transit.pages;
 
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import transit.system.Statistics;
 import transit.system.User;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
-import transit.system.TransitTime;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.YearMonth;
-import java.util.HashMap;
 
 /** Represents a page used to show statistical data to admin users */
 public class AdminGraphPage extends GraphPage {
 
   private User user;
 
+  private BorderPane layout = new BorderPane();
   /**
    * Initialized a new instance of AdminUserPage
    *
@@ -32,8 +29,13 @@ public class AdminGraphPage extends GraphPage {
   @Override
   void makeScene(Stage stage) {
     stage.setTitle("Transit System Simulator");
-    this.chart = makeSystemRevenueChart();
-    this.scene = new Scene(chart, 800, 600);
+    // the range of time you want the stats to cover
+    ComboBox<String> timeOptions = setupTimeOptions();
+    ComboBox<Statistics> statOptions = setupStatOptions(timeOptions);
+    HBox dropDowns = new HBox();
+    dropDowns.getChildren().addAll(timeOptions, statOptions);
+    layout.setTop(dropDowns);
+    this.scene = new Scene(layout, 800, 600);
   }
 
   /**
@@ -42,5 +44,30 @@ public class AdminGraphPage extends GraphPage {
    */
   public LineChart<String, Number> makeSystemRevenueChart() {
     return super.makeWeekChart(Statistics.getSystemRevenue().generateWeeklyValues());
+  }
+
+
+  public ComboBox<String> setupTimeOptions() {
+
+    ComboBox<String> timeOptions = new ComboBox<>();
+
+    timeOptions.getItems().addAll("Monthly", "Daily");
+    timeOptions.getSelectionModel().select(0);
+    return timeOptions;
+  }
+
+  public ComboBox<Statistics> setupStatOptions(ComboBox<String> timeOption) {
+    ComboBox<Statistics> statOptions = new ComboBox<>();
+    statOptions.getItems().addAll(Statistics.getSystemRevenue(), Statistics.getSystemTripLength());
+    statOptions.getSelectionModel().select(0);
+    statOptions.setOnAction(actionEvent -> {
+      if (timeOption.getValue().equals("Monthly")) {
+        chart = makeWeekChart(statOptions.getValue().generateWeeklyValues());
+      } else {
+        chart = makeYearChart(statOptions.getValue().generateMonthlyValues());
+      }
+      layout.setCenter(chart);
+    });
+    return statOptions;
   }
 }

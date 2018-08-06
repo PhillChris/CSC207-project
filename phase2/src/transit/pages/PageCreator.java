a4b927eb495eda147c3bf3712f889e38f10491cc
Fardin Syed <fardin.syed99@gmail.com>
2018-08-06 17:16:55 -0400

    Changed page creator inputs

--- phase2/src/transit/pages/PageCreator.java
+++ phase2/src/transit/pages/PageCreator.java
@@ -7,39 +7,31 @@ import java.util.HashMap;
 
 /** A factory to produce new pages */
 public class PageCreator {
-    private static Stage primaryStage;
-    private static Stage secondaryStage;
 
-    public static void setStages(Stage stage1, Stage stage2){
-        primaryStage = stage1;
-        secondaryStage = stage2;
+    void makeAddFundsPage(Card card){ new AddFundsPage(Main.primaryStage, card);}
+
+    void makeAdminUserPage(User adminUser){new AdminUserPage(Main.primaryStage, adminUser);}
+
+    void makeAnalyticsPage(Stage stage, HashMap<String, Statistics> statistics){
+        new AnalyticsPage(Main.secondaryStage, statistics);}
+
+    void makeAppendRoutePage(Route route){new AppendRoutePage(Main.secondaryStage, route);}
+
+    void makeCardPage(UserCardCommands cards){new CardPage(Main.secondaryStage, cards);}
+
+    void makeChangeNamePage(User user){new ChangeNamePage(Main.primaryStage, user);}
+
+    void makeChangePasswordPage(User user){new ChangeNamePage(Main.secondaryStage, user);}
+
+    void makeLoginPage(){new LoginPage(Main.primaryStage);}
+
+    void makeRouteCreationPage(){new RouteCreationPage(Main.secondaryStage);}
+
+    void makeSignUpPage(){new SignUpPage(Main.secogindaryStage);}
+
+    void makeTapPage(UserCardCommands cards, Card card, String selectedType){
+        new TapPage(Main.secondaryStage, cards, card, selectedType);
     }
 
-    void makeAddFundsPage(Stage primaryStage, Card card){ new AddFundsPage(primaryStage, card);}
-
-    void makeAdminUserPage(Stage primaryStage, User adminUser){new AdminUserPage(primaryStage, adminUser);}
-
-    void makeAnalyticsPage(HashMap<String, Statistics> statistics){new AnalyticsPage(statistics);}
-
-    void makeAppendRoutePage(Route route){new AppendRoutePage(route);}
-
-    void makeCardPage(Stage stage, UserCardCommands cards){new CardPage(stage, cards);}
-
-    void makeChangeNamePage(Stage primaryStage, User user){new ChangeNamePage(primaryStage, user);}
-
-    void makeChangePasswordPage(Stage primaryStage, User user){new ChangeNamePage(primaryStage, user);}
-
-    void makeLoginPage(Stage primaryStage){new LoginPage(primaryStage);}
-
-    void makeRouteCreationPage(Stage stage){new RouteCreationPage(stage);}
-
-    void makeSignUpPage(Stage stage){new SignUpPage(stage);}
-
-    void makeTapPage(Stage stage, UserCardCommands cards, Card card, String selectedType){
-        new TapPage(stage, cards, card, selectedType);
-    }
-
-    void makeTimeControlPage(Stage stage){new TimeControlPage(stage);}
-
-    void makeUserPage(Stage primaryStage, User user){new UserPage(primaryStage, user);}
+    void makeUserPage(User user){new UserPage(Main.primaryStage, user);}
 }

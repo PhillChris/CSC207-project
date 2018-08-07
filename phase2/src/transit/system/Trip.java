265e0f030c91843b212213121f085a2d922530b8
Fardin Syed <fardin.syed99@gmail.com>
2018-08-07 19:43:34 -0400

    Removed bug in calculating trip lengths

--- phase2/src/transit/system/Trip.java
+++ phase2/src/transit/system/Trip.java
@@ -155,31 +155,27 @@ public class Trip implements Serializable {
     Integer secondStationIndex = null;
 
     // Loop through all the routes
-    for (String type : Station.POSSIBLE_TYPES) {
-      if (Route.getRoutesCopy().get(type) != null) {
-        for (Route route : Route.getRoutesCopy().get(type)) {
-          // Do not check routes if the start and end station have already been found
-          if (firstStationIndex == null && secondStationIndex == null) {
-            // Loop through all the stations in a given route
-            for (int i = 0; i < route.getRouteStationsCopy().size(); i++) {
-              Station station = route.getRouteStationsCopy().get(i);
-              // Check equality in start station for this leg
-              if (station.equals(priorStops.get(priorStops.size() - 1))) {
-                firstStationIndex = i;
-              }
-              // Check for equality in the end station for this leg
-              if (station.equals(endStation)) {
-                secondStationIndex = i;
-              }
-            }
+    for (Route route :
+        Route.getRoutesCopy().get(priorStops.get(priorStops.size() - 1).getStationType())) {
+      // Do not check routes if the start and end station have already been found
+      if (firstStationIndex == null && secondStationIndex == null) {
+        // Loop through all the stations in a given route
+        for (int i = 0; i < route.getRouteStationsCopy().size(); i++) {
+          Station station = route.getRouteStationsCopy().get(i);
+          // Check equality in start station for this leg
+          if (station.equals(priorStops.get(priorStops.size() - 1))) {
+            firstStationIndex = i;
+          }
+          // Check for equality in the end station for this leg
+          if (station.equals(endStation)) {
+            secondStationIndex = i;
           }
         }
-
-        // If one of the two stations was not found in this route, set both indices to null
-        if (firstStationIndex == null || secondStationIndex == null) {
-          firstStationIndex = null;
-          secondStationIndex = null;
-        }
+      }
+      // If one of the two stations was not found in this route, set both indices to null
+      if (firstStationIndex == null || secondStationIndex == null) {
+        firstStationIndex = null;
+        secondStationIndex = null;
       }
     }
 

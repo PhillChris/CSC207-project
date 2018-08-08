69ec067a3d120992be608b63ecfcf1281c4878b6
Ren Zhang <rzhang0701@gmail.com>
2018-08-07 20:13:50 -0400

    Updated some Javadocs

--- phase2/src/transit/system/InvalidPasswordException.java
+++ phase2/src/transit/system/InvalidPasswordException.java
@@ -3,8 +3,12 @@ package transit.system;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
-
+/** Exception that gets thrown when a user enters an invalid password (less than 6 characters) */
 public class InvalidPasswordException extends MessageTransitException {
+  /**
+   * Updates the error label to tell the user that the given pass is invalid (less than 6 characters).
+   * @param errorMessage The error label being updated.
+   */
   public void setMessage(Label errorMessage) {
     errorMessage.setTextFill(Color.web("red"));
     errorMessage.setText("Invalid password");

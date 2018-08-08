be88394a6d6e7b8a2aaa32ef29abf44c98f1e406
PhillChris <chrph@utschools.ca>
2018-08-07 20:49:39 -0400

    Updates duties.txt

--- phase2/src/transit/system/LogWriter.java
+++ phase2/src/transit/system/LogWriter.java
@@ -198,7 +198,7 @@ public class LogWriter {
         .logInfoMessage(
             LoginPage.class.getName(),
             "parseLoginAttempt",
-            "Successfully logged in as + " + user.getPersonalInfo().getUserName());
+            "Successfully logged in as " + user.getPersonalInfo().getUserName());
   }
 
   /** Logs an invalid authorization */
